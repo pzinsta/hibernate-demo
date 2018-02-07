4ab95f46bc2ce523e9daa41a51b8da3a1476e18b
pzinsta <voodooshake@gmail.com>
2018-02-07 19:01:28 +0200

    Add object removal demo

---
+++ src/main/java/com/pzinsta/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,43 @@
+package com.pzinsta.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.pzinsta.hibernate.demo.entity.Student;
+
+public class DeleteStudentDemo {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class).buildSessionFactory();
+        
+        
+        try {
+
+            int studentID = 1;
+            Session session = sessionFactory.getCurrentSession();
+            
+            session.beginTransaction();
+            
+            System.out.println("getting student with id: " + studentID);
+            
+            Student student = session.get(Student.class, studentID);
+            
+            session.getTransaction().commit();
+            
+            
+            session = sessionFactory.getCurrentSession();
+            
+            session.beginTransaction();
+            
+            session.delete(student);
+            
+            session.getTransaction().commit();
+        }
+        finally {
+            sessionFactory.close();
+        }
+    }
+
+}
